243943a02d4c95bdfccf00988fc2d8b4f4f58027
Qaisar <kaiserbashirahanger06@gmail.com>
2022-05-10 14:41:28 +0530

    First Comit

---
+++ src/com/company/DSP3.java
@@ -0,0 +1,38 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class DSP3 {
+    /*Given an array of integers, find the first missing positive integer in linear time and constant space.
+     In other words, find the lowest positive integer that does not exist in the array.
+      The array can contain duplicates and negative numbers as well.
+        For example, the input [3, 4, -1, 1] should give 2. The input [1, 2, 0] should give 3.*/
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int n=sc.nextInt();
+        int []arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        System.out.println(MissingNumber(arr,n));
+    }
+    public static int MissingNumber(int arr[],int n){
+        int hash[]=new int[10000001];
+        for(int i=0;i<n;i++){
+            if(arr[i]>0){
+                hash[arr[i]]=1;
+            }
+        }
+        int j=1;
+        while(j<n){
+            if(hash[j]==0){
+                return j;
+            }
+            j++;
+        }
+        if(j==n){
+            return n;
+        }
+        return -1;
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,12 +6,13 @@ import java.util.Scanner;
 
 public class Main {
 
+    /*Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
+        For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.*/
     public static void main(String[] args) {
 	// write your code here
         Scanner sc= new Scanner(System.in);
 
-       //.........................First problem
-    /*    int n=sc.nextInt();
+        int n=sc.nextInt();
         int k= sc.nextInt();
         ArrayList<Integer> arr=new ArrayList<>();
         for(int i=0;i<n;i++){
@@ -22,17 +23,8 @@ public class Main {
             System.out.println("True");
         }else {
             System.out.println("False");
-        }*/
-
-     //..........................Second Problem
-        int n=sc.nextInt();
-        int arr[]=new int[n];
-        for(int i=0;i<n;i++){
-            arr[i]=sc.nextInt();
         }
-        prodArray(arr,n);
     }
-    // first problem
     public static boolean twoSum(ArrayList<Integer> arr,int k){
         int n = arr.size();
         HashMap<Integer,Integer> map = new HashMap<>();
@@ -44,25 +36,5 @@ public class Main {
             map.put(arr.get(i),i);
         }
         return false;
-    }
-    //second problem
-    public static void prodArray(int arr[],int n){
-        int left[]=new int[n];
-        int right[]=new int[n];
-        int prod[]=new int[n];
-        left[0]=1;
-        right[n-1]=1;
-        for(int i=1;i<n;i++){
-            left[i]=arr[i-1]*left[i-1];
-        }
-        for(int j=n-2;j>=0;j--){
-            right[j]=arr[j+1]*right[j+1];
-        }
-        for(int i=0;i<n;i++){
-            prod[i]=left[i]*right[i];
-        }
-        for(int i=0;i<n;i++){
-            System.out.println(prod[i]+" ");
-        }
     }
 }
