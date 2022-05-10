19860c2fce474034b023119a7f993a0eda28c4d3
Qaisar <kaiserbashirahanger06@gmail.com>
2022-05-10 14:45:27 +0530

    First Comit

--- src/com/company/DCP3.java
+++ src/com/company/DCP3.java
@@ -2,11 +2,11 @@ package com.company;
 
 import java.util.Scanner;
 
-public class DSP3 {
+public class DCP3 {
     /*Given an array of integers, find the first missing positive integer in linear time and constant space.
-     In other words, find the lowest positive integer that does not exist in the array.
-      The array can contain duplicates and negative numbers as well.
-        For example, the input [3, 4, -1, 1] should give 2. The input [1, 2, 0] should give 3.*/
+    In other words, find the lowest positive integer that does not exist in the array.
+     The array can contain duplicates and negative numbers as well.
+       For example, the input [3, 4, -1, 1] should give 2. The input [1, 2, 0] should give 3.*/
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
         int n=sc.nextInt();
