922f90393ae514f114059c2fa2f42b6f66f10d3b
Qaisar <kaiserbashirahanger06@gmail.com>
2022-06-06 16:07:05 +0530

    First Comit

---
+++ src/com/company/DCP12.java
@@ -0,0 +1,36 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class DCP12 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n = sc.nextInt();
+        int k=sc.nextInt();
+        int []arr = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        int l[]=new int[2];
+        l=printPowerSets(arr,n,k);
+        for(int j=l[0];j<l[1];j++){
+            System.out.println(arr[j]);
+        }
+    }
+    public static int[] printPowerSets(int arr[],int n,int k){
+        int l[] = new int[n];
+        int total = 0;
+        for(int i=0;i<n;i++){
+            total = arr[i];
+            for(int j=i+1;j<n;j++){
+                total+=arr[j];
+                if(total==k){
+                    l[0]=i;
+                    l[1]=j+1;
+                    return l;
+                }
+            }
+        }
+        return l;
+    }
+}
